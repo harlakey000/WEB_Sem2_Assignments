a9d6c9dd0cd5cc50b16ca38bf77f89753877081e
Keyera Harlan <harlakey000@warren.k12.in.us>
2024-02-05 09:02:04 -0500

    Unit 04 lesson 02

--- Unit 04_Objects, Events, Strings, and Numbers/02_Events Basics/script.js
+++ Unit 04_Objects, Events, Strings, and Numbers/02_Events Basics/script.js
@@ -1,18 +1,4 @@
-function mouseOverHandler() {
-    document.getElementById("demo").innerHTML = "Hello Again"
-}
 
 function mouseOutHandler() {
-    document.getElementById("kitties").hidden = false;
-}
-
-function keyDownHandler() {
-    let x = 5
-    let y = x + 7
-    document.getElementById("demo").innerHTML = y
-    
-}
-
-function keyUpHandler() {
-    document.getElementById("kitties").hidden = true;
+    document.getElementById("HIM").hidden = True;
 }
