be1d5c89b55ed8ed2ed57cd487f09abf283dd293
Keyera Harlan <harlakey000@warren.k12.in.us>
2024-02-05 09:07:31 -0500

    Unit 04 lesson 03

---
+++ Unit 04_Objects, Events, Strings, and Numbers/03_String Basics/script.js
@@ -0,0 +1,25 @@
+I love you
+Three words I don't say anymore
+Maybe because my heart is torn
+It's taped up and it's stapled
+I don't know if I'm able
+
+To move on as I should
+I'm watching you leave again
+And it replays in my head
+A million times and I can't take it
+My heart is breaking once more
+
+Picking my mind up off the floor
+Will I put it back in my skull?
+No, not ever
+'Cause you said we were forever
+Huh, that's some fable
+
+But I can't move on as I should
+I'm watching you leave again
+And it replays in my head
+A million times and I can't take it
+My heart is breaking once more
+
+document.getElementById("lyrics").innerHTML = lyrics;
