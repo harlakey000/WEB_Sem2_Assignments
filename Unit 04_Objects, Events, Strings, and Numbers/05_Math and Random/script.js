4aceacef33bdb8b1f0e80c32a92a5606dbcc03cf
Keyera Harlan <harlakey000@warren.k12.in.us>
2024-02-06 09:06:37 -0500

    Unit 04 lesson 05

---
+++ Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
@@ -0,0 +1,7 @@
+function takeSquareRoot() {
+    let number1 = +document.getElementById("input1").value;
+    
+
+    let squareRoot = Math.sqrt(number)
+    document.getElementById("result").innerHTML = sqareRoot;
+}
