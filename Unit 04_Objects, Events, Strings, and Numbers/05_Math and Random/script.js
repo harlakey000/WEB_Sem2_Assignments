1115e906be8bcca0b93881b372f5c2db063dcf60
Keyera Harlan <harlakey000@warren.k12.in.us>
2024-02-07 08:45:23 -0500

    Unit 04 lesson 05 assignment 02

--- Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
+++ Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
@@ -4,4 +4,13 @@ function takeSquareRoot() {
 
     let squareRoot = Math.sqrt(number)
     document.getElementById("result").innerHTML = sqareRoot;
+
+    let absolutevalue = Math.absolutevalue(number)
+    document.getElementById("result").innerHTML = absolutevalue;
+
+    let rounddown = Math.rounddown(number)
+    document.getElementById("result").innerHTML = rounddown;
+
+    let sin = Math.sin(number)
+    document.getElementById("result").innerHTML = sin
 }
